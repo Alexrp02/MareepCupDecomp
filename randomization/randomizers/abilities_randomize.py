219b7d5b593cdedc8669d906f0f34f2b14be4a6a
AlexRP <aleeexrp02@gmail.com>
2024-02-22 09:40:23 +0100

    Indented code in abilities randomizer

--- randomization/randomizers/abilities_randomize.py
+++ randomization/randomizers/abilities_randomize.py
@@ -1,14 +1,16 @@
 import random
 
-with open ("data/ability_define.txt", "r") as f:
+with open("data/ability_define.txt", "r") as f:
     ability_pool = f.readlines()
-for i in range(1,10):
-    
-# Read your .h file line by line
-    with open(f"../src/data/pokemon/species_info/gen_{i}.h", "r", encoding="utf-8") as file:
+for i in range(1, 10):
+
+    # Read your .h file line by line
+    with open(
+        f"../src/data/pokemon/species_info/gen_{i}.h", "r", encoding="utf-8"
+    ) as file:
         lines = file.readlines()
 
-# Iterate through each line
+    # Iterate through each line
     new_lines = []
     # Variable to check if geodude family
     geodude = False
@@ -21,13 +23,15 @@ for i in range(1,10):
         if line.startswith("        .abilities = {"):
             # Extract the current abilities list
             abilities_string = line.split("=")[1].strip().split("}")[0].strip()
-            abilities_list = [ability.strip() for ability in abilities_string.split(",")]
+            abilities_list = [
+                ability.strip() for ability in abilities_string.split(",")
+            ]
 
             new_abilities_list = random.sample(ability_pool, len(abilities_list))
             new_abilities_list = [ability.strip() for ability in new_abilities_list]
 
             # Update the line with the new abilities list
-            new_line = "        .abilities = {" + ', '.join(new_abilities_list) + "}"
+            new_line = "        .abilities = {" + ", ".join(new_abilities_list) + "}"
             if not geodude:
                 new_line = new_line + ",\t\\\n"
             else:
@@ -41,8 +45,10 @@ for i in range(1,10):
         # Add the updated line to the new list
         new_lines.append(new_line)
 
-# Write the modified lines back to the file
-    with open(f"../src/data/pokemon/species_info/gen_{i}.h", "w", encoding="utf-8") as file:
+    # Write the modified lines back to the file
+    with open(
+        f"../src/data/pokemon/species_info/gen_{i}.h", "w", encoding="utf-8"
+    ) as file:
         file.writelines(new_lines)
 
     print(f"Abilities successfully randomized in gen{i}.h!")
